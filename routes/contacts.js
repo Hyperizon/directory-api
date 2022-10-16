38e2ffe8a8f33d7e61640f53757f72114cb8b7ca
Mehmetcan Kaplan <hyperizon@hotmail.com>
2022-10-16 20:19:23 +0300

    contacts crud options

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,10 +7,35 @@ const uploadImage = require('../util/imageUploader');
 
 
 router.get('/', verifyToken, (req, res) => {
-    Contact.findAll({where: {userId: JWT.verify(req.header('auth-token'), process.env.TOKEN_SECRET).id}}).then(result => {
+    Contact.findAll({
+        where: {
+            userId: JWT.verify(req.header('auth-token'), process.env.TOKEN_SECRET).id,
+            deletedAt: null
+        }
+    }).then(result => {
         res.send(result);
     }).catch(() => res.send("No contact"));
 
+});
+
+router.get('/get/:id', verifyToken, (req, res) => {
+    const validation = joi.object({
+        id: joi.number()
+            .positive()
+            .integer()
+            .required()
+    }).validate(req.params);
+    if (validation.error) return res.status(404).send(validation.error.details[0].message)
+
+    Contact.findOne({
+        where: {
+            userId: JWT.verify(req.header('auth-token'), process.env.TOKEN_SECRET).id,
+            deletedAt: null,
+            id: req.params.id
+        }
+    }).then(result => {
+        res.send(result);
+    }).catch(() => res.send("No contact"));
 });
 
 router.post('/create', uploadImage.upload.single('image'), verifyToken, (req, res) => {
@@ -48,13 +73,18 @@ router.post('/create', uploadImage.upload.single('image'), verifyToken, (req, re
 });
 
 router.delete('/delete/:id', verifyToken, (req, res) => {
-    const id = req.params.id;
-    if (!id) return res.status(404).send('The "id" parameter cannot be empty!')
+    const validation = joi.object({
+        id: joi.number()
+            .positive()
+            .integer()
+            .required()
+    }).validate(req.params);
+    if (validation.error) return res.status(404).send(validation.error.details[0].message)
 
     Contact.destroy({
         where: {
             userId: JWT.verify(req.header('auth-token'), process.env.TOKEN_SECRET).id,
-            id: id
+            id: req.params.id
         }
     }).then((result) => {
         if (!result) res.status(404).send('Deleted unsuccessfully! Check "id" parameter.')
@@ -63,7 +93,68 @@ router.delete('/delete/:id', verifyToken, (req, res) => {
 });
 
 router.patch('/restore/:id', verifyToken, (req, res) => {
-    res.send(req.params.id);
+    const validation = joi.object({
+        id: joi.number()
+            .positive()
+            .integer()
+            .required()
+    }).validate(req.params);
+    if (validation.error) return res.status(404).send(validation.error.details[0].message);
+
+    Contact.restore({
+        where: {
+            userId: JWT.verify(req.header('auth-token'), process.env.TOKEN_SECRET).id,
+            id: req.params.id
+        }
+    }).then((result) => {
+        if (!result) res.status(404).send('Restore unsuccessfully! Check "id" parameter.')
+        else res.status(200).send("Restore successfully")
+    }).catch(err => res.status(404).send(err.message));
+});
+
+router.patch('/update/:id', uploadImage.upload.single('image'), verifyToken, (req, res) => {
+    const validation1 = joi.object({
+        id: joi.number()
+            .positive()
+            .integer()
+            .required()
+    }).validate(req.params);
+    if (validation1.error) return res.status(404).send(validation1.error.details[0].message);
+
+    const validation2 = joi.object({
+        name: joi.string()
+            .max(25),
+        lastname: joi.string()
+            .max(25),
+        company: joi.string()
+            .max(25),
+        email: joi.string()
+            .email()
+            .max(100),
+        phoneNumber: joi.string()
+            .regex(/^\+?[0-9][0-9]{7,14}$/, {name: 'phoneNumber'})
+            .max(14),
+        image: joi.string()
+            .max(255)
+    }).validate(req.body);
+    if (validation2.error) return res.status(404).send(validation2.error.details[0].message);
+
+    Contact.update({
+        name: req.body.name,
+        lastname: req.body.lastname,
+        company: req.body.company,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        image: (req.file) ? req.file.path : null,
+    }, {
+        where: {
+            userId: JWT.verify(req.header('auth-token'), process.env.TOKEN_SECRET).id,
+            id: req.params.id
+        }
+    }).then((result) => {
+        if (!result) res.status(404).send('Restore unsuccessfully! Check "id" parameter.')
+        else res.status(200).send("Restore successfully")
+    }).catch(err => res.status(404).send(err.message));
 });
 
 
