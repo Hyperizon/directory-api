9473d5db1821ff990ca0adec84dca59047334856
Mehmetcan Kaplan <hyperizon@hotmail.com>
2022-10-18 11:15:42 +0300

    Update Contact.js

--- model/Contact.js
+++ model/Contact.js
@@ -3,7 +3,7 @@ const db = require('../util/database');
 
 const contactScheme = db.define('contacts', {
     name: {type: DataTypes.STRING(25)},
-    lastname: {type: DataTypes.STRING(25)},
+    lastName: {type: DataTypes.STRING(25)},
     company: {type: DataTypes.STRING(25)},
     email: {type: DataTypes.STRING(100)},
     phoneNumber: {type: DataTypes.STRING(14)},
