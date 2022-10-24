3a29b76294579d3b398e12963c3a7cc2ec5378d6
Mehmetcan Kaplan <hyperizon@hotmail.com>
2022-10-24 10:27:51 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -57,7 +57,7 @@ router.post('/login', async (req, res) => {
     if (!validPass) return res.status(404).send('Invalid Password');
 
     const token = JWT.sign({id: user.id}, process.env.TOKEN_SECRET)
-    res.send({token: token});
+    res.send({token: token, userId: user.id, userName: user.name, userLastName: user.lastName});
 });
 
 router.post('/checkToken', async (req, res) => {
